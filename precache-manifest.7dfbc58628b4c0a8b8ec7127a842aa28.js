84f4d909f826591ad560a4e642e4da79f74bf096
Amri <chozinul@gmail.com>
2019-04-27 19:42:54 +0800

    Updates

---
+++ precache-manifest.7dfbc58628b4c0a8b8ec7127a842aa28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdc8e2f554a520b467b0",
+    "url": "/ReduxTutorial/static/js/runtime~main.57d90c48.js"
+  },
+  {
+    "revision": "5146abc34698d21a69fb",
+    "url": "/ReduxTutorial/static/js/main.a071a209.chunk.js"
+  },
+  {
+    "revision": "e03636201cd3e2bbaebb",
+    "url": "/ReduxTutorial/static/js/2.d3bd651a.chunk.js"
+  },
+  {
+    "revision": "5146abc34698d21a69fb",
+    "url": "/ReduxTutorial/static/css/main.6466f31e.chunk.css"
+  },
+  {
+    "revision": "52006c4ba0ea9ac99820e26af0e5f4b5",
+    "url": "/ReduxTutorial/index.html"
+  }
+];
